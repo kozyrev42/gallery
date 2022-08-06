c78c2137a5475fbb4a9b1eaf233055c4917271ff
kozyrev42 <kozyrev054@gmail.com>
2022-08-06 21:45:04 +0700

    мелкие правки

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,80 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function about(){
-        return view('about');
+        // рендер страниц about.blade.php
+        //return view('about');
+
+        // получаем массив из Базы
+        $users = [
+            [
+                "id" => 1,
+                "name" => "jon",
+                "status" => "active",
+                "sex" => "m"
+            ],
+            [
+                "id" => 2,
+                "name" => "san",
+                "status" => "ban",
+                "sex" => "m"
+            ],
+            [
+                "id" => 3,
+                "name" => "dash",
+                "status" => "ban",
+                "sex" => "w"
+            ],
+            [
+                "id" => 4,
+                "name" => "kate",
+                "status" => "active",
+                "sex" => "w"
+            ],
+        ];
+
+        
+        $bannedUsers = [];
+        $mUsers = [];
+
+        // обчный способ, получить только забаненных юзеров
+        foreach ($users as $user) {
+            if($user["status"] == "ban" ) {
+                // в массив кладём только забаненых
+                $bannedUsers[] = $user;
+                // из забаненных, только женщин
+                if($user["sex"] == "w") {
+                    $mUsers[] = $user;
+                }
+            }
+        }
+
+        // оборачиваем в Экземпляр Класс Collection
+        $collectUsers = collect($users);
+        //dd($collectUsers);
+
+        // map() - перебирает $collectUsers, и передаёт в колбэк каждый элемент
+        // далее с каждым элементом можно поработать
+        // используем метод map() для выборки "name"
+        // возвращается массив только с именами юзеров
+        // по итогу формируется новый массив
+        $name = $collectUsers->map(function ($user) {
+            return $user['name'];
+        });
+
+        // filter() - колбэк возвращает элементы прошедшие по критерию отбора
+        // хотим вернуть только элементы, в которых "sex" => "m"
+        $sexM = $collectUsers->filter(function ($user) {
+            return $user['sex'] == 'm';
+        });
+
+
+        // можно применять цепочки вызовов
+        $sexMban = $collectUsers->filter(function ($user) {
+            return $user['sex'] == 'm';
+        })->filter(function ($user) {
+            return $user['status'] == 'ban';
+        });
+
+        dd($sexMban);
     }
 }
